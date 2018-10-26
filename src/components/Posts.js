cb75d273f417ba7998d3d8ae2b1dfc66e040ae59
tuhnik <huntlarsen@gmail.com>
2018-10-26 06:40:56 +0300

    css

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -3,26 +3,26 @@ import Card from './Card'
 import Loading from './Loading'
 class Posts extends Component {
   
-state={genre: 1, page: 1, data: [], error: null, loading: false}
+state={genre: 1, page: 1, totalpages: 1,data: [], error: null, loading: false}
 getPosts(genre, page){
 
     
     this.setState({loading: true})
 
     fetch(`https://api.jikan.moe/v3/genre/manga/${genre}/${page}`).then(res=>res.json())
-    .then(res=>{    
+    .then(res=>{
        let page = this.state.page + 1
        let data = this.state.data
-       this.setState({data: [...data, ...res.manga], loading: false, page})
+       console.log(Math.ceil(res.item_count/100))
+       this.setState({data: [...data, ...res.manga], loading: false, page, totalpages: Math.ceil(res.item_count/100)})
     })
     .catch(err=>{
-        // this.setState({data: null, after: null, subreddit: null, error: err, loading: false})
+        this.setState({data: null, after: null, subreddit: null, error: err, loading: false})
     })
 }
 
 
 componentWillReceiveProps (nprops){
-    console.log(nprops)
     if(this.state.genre !== nprops.genre){
         this.setState({genre: nprops.genre, data: [], error: null, page: 1}, ()=>{
             this.getPosts(nprops.genre, 1)
@@ -42,7 +42,9 @@ handleScroll(e){
     var pageOffset = window.pageYOffset + window.innerHeight
     var bottomOffset = 20
     if (pageOffset > lastPostOffset - bottomOffset) {
-       this.getPosts(this.state.genre, this.state.page)
+        if(this.state.page < this.state.totalpages){
+            this.getPosts(this.state.genre, this.state.page)
+        }  
     }
 }
 render() {
