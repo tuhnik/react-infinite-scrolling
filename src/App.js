72bbade41c8989a32c0aab4ec78944267be0c0b6
tuhnik <huntlarsen@gmail.com>
2018-10-27 00:07:35 +0300

    background

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Genres from './components/Genres'
 import Cards from './components/Cards'
-import Input from './components/Input'
+
 class App extends Component {
   state = {genre: 1}
   submitted(id){
@@ -14,8 +14,7 @@ class App extends Component {
     return (
   
       <div className="App">
-        {/* <Input callback={this.submitted.bind(this)}/> */}
-        <h3 className="header">🍙 Manga toplist</h3>
+        <h3 className="header"> <span role="img" aria-label="riceball">🍙</span> Manga toplist</h3>
         <Genres genre={this.state.genre} changeGenre={this.changeGenre.bind(this)}/>
         <Cards genre={this.state.genre} changeGenre={this.changeGenre.bind(this)}/>
       </div>
