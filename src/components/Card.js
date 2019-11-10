02af3ef75155f35fee158c376a1dc15e38c2c983
tuhnik <tuhnikbot@gmail.com>
2019-11-10 06:58:29 +0200

    copy task

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,11 +4,16 @@ class Card extends Component {
     const {el, changeGenre, genreID} = this.props
     return <div className="card">
         <div className="card-image">
+        <a href={el.url}>
           <img src={el.image_url} alt={el.title} />
+          </a>
         </div>
         <div className="card-text">
           <div className="title-container">
-            <div className="title">{el.title}</div>
+            <div className="title">
+            <a href={el.url}>{el.title}</a>
+            {el.volumes?<span className="volumes"> ({el.volumes}&nbsp;volumes)</span>:""}
+            </div>
             {el.members && <div className="members"><span>{el.members}</span></div>}
             {el.score && <div className="score"><span>{el.score}</span></div> }      
           </div>
